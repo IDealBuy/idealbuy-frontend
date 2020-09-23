c52afe42360a5120659ca529fa8f94f2d146e568
Santiago Forero <ds.foreroa@uniandes.edu.co>
2020-09-23 00:10:23 -0500

    add global styles to app

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,25 +1,9 @@
 import { createGlobalStyle } from 'styled-components'
-import KarlaRegular from "./assets/fonts/Karla-Regular.ttf";
-import KarlaBold from "./assets/fonts/Karla-Bold.ttf";
-import KarlaItalic from "./assets/fonts/Karla-Italic.ttf";
-
+import KarlaRegular from './assets/fonts/Karla-Regular.ttf'
+import KarlaBold from './assets/fonts/Karla-Bold.ttf'
+import KarlaItalic from './assets/fonts/Karla-Italic.ttf'
 
 const GlobalStyle = createGlobalStyle`
-
-  :root{
-      --primary-color: #E63946;
-      --primary-color50: #F39BA2;
-      --secondary-color:#1D3557;
-      --secondary-color50:#6C95CE;
-      --background-color:#F1FAEE;
-      --background-color50:#F8FDF7;
-  }
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: Open-Sans, Helvetica, Sans-Serif;
-  }
-  
   @font-face {
         font-family: 'Karla';
         src:url(${KarlaRegular}) format('ttf');
@@ -38,6 +22,23 @@ const GlobalStyle = createGlobalStyle`
         font-weight: normal;
         font-style: italic;
     }
+
+  :root{
+      --primary-color: #E63946;
+      --primary-color50: #F39BA2;
+      --secondary-color:#1D3557;
+      --secondary-color50:#6C95CE;
+      --background-color:#F1FAEE;
+      --background-color50:#F8FDF7;
+      --primary-font:"Karla", sans-serif;
+
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: var(--primary-font);
+  }
+  
  
 `
 
