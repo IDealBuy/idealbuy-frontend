8ac29d91c1d3876a31360bab9c632e08a4b8be9f
Santiago Forero <ds.foreroa@uniandes.edu.co>
2020-09-23 00:03:13 -0500

    global styles created

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Buttons = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export const RectangularButton = () => (
+    <button>
+        
+    </button>
+)
---
+++ src/globalStyles.js
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components'
+import KarlaRegular from "./assets/fonts/Karla-Regular.ttf";
+import KarlaBold from "./assets/fonts/Karla-Bold.ttf";
+import KarlaItalic from "./assets/fonts/Karla-Italic.ttf";
+
+
+const GlobalStyle = createGlobalStyle`
+
+  :root{
+      --primary-color: #E63946;
+      --primary-color50: #F39BA2;
+      --secondary-color:#1D3557;
+      --secondary-color50:#6C95CE;
+      --background-color:#F1FAEE;
+      --background-color50:#F8FDF7;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+  }
+  
+  @font-face {
+        font-family: 'Karla';
+        src:url(${KarlaRegular}) format('ttf');
+        font-weight: normal;
+        font-style: normal;
+    }
+  @font-face {
+        font-family: 'Karla';
+        src:url(${KarlaBold}) format('ttf');
+        font-weight: bold;
+        font-style: normal;
+    }
+  @font-face {
+        font-family: 'Karla';
+        src:url(${KarlaItalic}) format('ttf');
+        font-weight: normal;
+        font-style: italic;
+    }
+ 
+`
+
+export default GlobalStyle
